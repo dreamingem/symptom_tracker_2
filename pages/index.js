2e2b4061c90d4ddc01f059de84ca6db58d11e796
dreamingem <gemma.stone.k@gmail.com>
2025-08-15 02:00:35 +0900

    Update index.js
    head 추가

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
 import SymptomTracker from '../components/SymptomTracker'
 
 export default function Home() {
